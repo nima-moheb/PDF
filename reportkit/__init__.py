b9092375508bd4dee8e4a9c458390ad9834fd6b9
Nima <34025581+nima-moheb@users.noreply.github.com>
2026-09-22 10:31:03 +0330

    v0.5: reportkit/__init__.py

--- reportkit/__init__.py
+++ reportkit/__init__.py
@@ -1,1 +1,8 @@
-from .engine import build
+from . import engine as _engine
+from .visual_v05 import install as _install_visual_v05
+
+_install_visual_v05()
+
+build = _engine.build
+
+__all__ = ["build"]
