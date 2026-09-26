b77a76f3f5ad32bc3f29d5066a194cdf500392f1
Nima <34025581+nima-moheb@users.noreply.github.com>
2026-09-26 13:45:57 +0330

    v0.6.2: install adaptive metric/font layer

--- reportkit/__init__.py
+++ reportkit/__init__.py
@@ -2,10 +2,12 @@ from . import engine as _engine
 from .visual_v05 import install as _install_visual_v05
 from .visual_v06 import install as _install_visual_v06
 from .delivery_v061 import install as _install_delivery_v061
+from .visual_v062 import install as _install_visual_v062
 
 _install_visual_v05()
 _install_visual_v06()
 _install_delivery_v061()
+_install_visual_v062()
 
 build = _engine.build
 
